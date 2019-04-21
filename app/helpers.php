76b26869241dbd95bb9e533124eba5d4f87c282b
luoxu <luoxulx@live.com>
2019-04-21 20:09:28 +0800

    help text

--- app/helpers.php
+++ app/helpers.php
@@ -1,2 +1,61 @@
 <?php
 
+if (!function_exists('curlRequest')) {
+    /**
+     * @param $url
+     * @param array $params
+     * @param string $method
+     * @param array $header
+     * @return mixed
+     * @throws Exception
+     */
+    function curlRequest($url, $params = [], $method = 'GET', $header = ['Content-Type: application/json; charset=utf-8'])
+    {
+        $opts = array(
+            CURLOPT_TIMEOUT        => 30,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_HTTPHEADER     => $header
+        );
+        /* 根据请求类型设置特定参数 */
+        switch(strtoupper($method)){
+            case 'GET':
+                if ($params){
+                    $opts[CURLOPT_URL] = $url . '?' . http_build_query($params);
+                }else {
+                    $opts[CURLOPT_URL] = $url;
+                }
+                break;
+            case 'POST':
+                $opts[CURLOPT_URL] = $url;
+                $opts[CURLOPT_POST] = 1;
+                $opts[CURLOPT_POSTFIELDS] = http_build_query($params);
+                break;
+            case 'PUT' :
+                $opts[CURLOPT_URL] = $url;
+                $opts[CURLOPT_CUSTOMREQUEST] = 'PUT';
+                $opts[CURLOPT_POSTFIELDS]      = http_build_query($params);
+                break;
+            case 'DELETE' :
+                $opts[CURLOPT_URL] = $url;
+                $opts[CURLOPT_CUSTOMREQUEST] = 'DELETE';
+                $opts[CURLOPT_POSTFIELDS] = http_build_query($params);
+                break;
+            default:
+                throw new Exception('Unsupported mode of request!');
+        }
+
+        /* 初始化并执行curl请求 */
+        $ch = curl_init();
+        curl_setopt_array($ch, $opts);
+        $data  = curl_exec($ch);
+        $error = curl_error($ch);
+        curl_close($ch);
+
+        if($error) {
+            return ['code'=>9999,'error'=>$error,'message'=>'failed'];
+        }
+        return ['code'=>0, 'message'=>'successful','result'=>json_decode($data, true), 'request'=>$params];
+    }
+}
