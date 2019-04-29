60380fce803265802c9805fcb8ad06bdafa5978b
luoxu <luoxulx@live.com>
2019-04-29 20:53:15 +0800

    fixed

---
+++ app/Http/Controllers/Auth/AuthGithubController.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luoxulx
+ * Date: 2019/4/29
+ * Time: 下午5:41
+ */
+
+namespace App\Http\Controllers\Auth;
+
+
+use App\Http\Controllers\Controller;
+use Laravel\Socialite\Facades\Socialite;
+
+
+class AuthGithubController extends Controller
+{
+
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('github')->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+    {
+        $user = Socialite::driver('github')->user();
+
+        // $user->token;
+    }
+}
