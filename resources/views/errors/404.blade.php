5b8be55946edb7f5ef5790e27d656372a09bbfe1
luoxu <luoxulx@live.com>
2019-08-09 15:11:26 +0800

    errors page

--- resources/views/errors/404.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('errors::layout')
-
-@section('title', '404 Page Not Found')
-@section('message', '404 Page Not Found')
--- resources/views/errors/50x.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('errors::layout')
-
-@section('title', '50x Server Error')
-@section('message', '50x Server Error')
