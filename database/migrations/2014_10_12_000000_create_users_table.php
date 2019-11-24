d3e56de21914fa5e934496df33f03c387b3291ce
14k <lx@dell.com>
2019-11-24 18:32:20 +0800

    fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('nickname',100)->nullable()->default('')->comment('账户昵称');
+            $table->string('nickname',100)->default('')->comment('账户昵称');
             $table->string('name', 50)->unique()->comment('账户名称,可用作登录');
             $table->string('email', 128)->unique()->comment('账户email,可用作登录');
             $table->string('password');
